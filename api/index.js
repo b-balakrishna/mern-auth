9e01032afda9b517ebfed471eec1ec1f085709b9
b-balakrishna <battulabalakrishna063@gmail.com>
2023-10-08 23:28:11 +0530

    Connected to DB

--- api/index.js
+++ api/index.js
@@ -1,7 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const app = express();
+const app = express(process.env.MONGODB_URI)
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((err) => {
+        console.log('Error Connecting to MongoDB', err);
+    });
 
+mongoose.connect();
 app.listen(5000, () => {
     console.log('listening on port 5000');
 });
