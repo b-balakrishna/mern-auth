f3b4b232feaf99dd6c967d1960053fd9a472eb38
b-balakrishna <battulabalakrishna063@gmail.com>
2023-10-08 20:41:56 +0530

    added routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import SignUp from './pages/SignUp';
+import Login from './pages/Login';
+
 const App = () => {
     return (
-        <div>
-            <h1>Hello</h1>
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/sign-in' element={<Login />} />
+                <Route path='/about' element={<About />} />
+                <Route path='/profile' element={<Profile />} />
+                <Route path='/sign-up' element={<SignUp />} />
+            </Routes>
+        </BrowserRouter>
     );
 };
 export default App;
