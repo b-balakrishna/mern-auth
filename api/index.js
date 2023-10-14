c96d6d24f808929ddd1dd6148d92c07996e4e03f
b-balakrishna <battulabalakrishna063@gmail.com>
2023-10-14 15:18:54 +0530

    added signup

--- api/index.js
+++ api/index.js
@@ -3,8 +3,11 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 
 const app = express();
+
+app.use(express.json());
 
 mongoose
     .connect(process.env.MONGODB_URI)
@@ -20,3 +23,4 @@ app.listen(5000, () => {
 });
 
 app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
