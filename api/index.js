9df667b882df51c892d6a3d36bd1b99b0af5c4c0
b-balakrishna <battulabalakrishna063@gmail.com>
2024-02-03 21:39:15 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ import cors from 'cors';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
-import path from 'path';
+import { __dirname } from 'path';
 
 const app = express();
 
